5410e44964f52f9ec79b36c513bd76d651dda30d
malin <mal3eine@hotmail.com>
2022-12-14 21:08:52 +0100

    Fixat mer mergefel

--- HotelBooking/Controllers/Menu/ManageCustomerMenu.cs
+++ HotelBooking/Controllers/Menu/ManageCustomerMenu.cs
@@ -6,24 +6,24 @@ public class ManageCustomerMenu
 {
     public static void CustomerMenu(int selection)
     {
-        if (selection == 0)
-        {
-            //avsluta
-        }
-        else if (selection == 1)
-        {
-            ManageCustomer manage = new ManageCustomer();
-            var customer = manage.CreateCustomer();
-            manage.SaveCustomerToList(customer);
-            manage.SaveCustomerToDatabase();
-        }
-        else if (selection == 2)
-        {
-            //ändra kunduppgifter
-        }
-        else if (selection == 3)
-        {
-            //avregistrera kund
-        }
+        //if (selection == 0)
+        //{
+        //    //avsluta
+        //}
+        //else if (selection == 1)
+        //{
+        //    ManageCustomer manage = new ManageCustomer();
+        //    var customer = manage.CreateCustomer();
+        //    manage.SaveCustomerToList(customer);
+        //    manage.SaveCustomerToDatabase();
+        //}
+        //else if (selection == 2)
+        //{
+        //    //ändra kunduppgifter
+        //}
+        //else if (selection == 3)
+        //{
+        //    //avregistrera kund
+        //}
     }
 }
