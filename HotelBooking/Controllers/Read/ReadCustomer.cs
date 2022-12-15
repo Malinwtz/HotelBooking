9e237da0a298f6e825039c0323ec54e856f30da0
malin <mal3eine@hotmail.com>
2022-12-15 14:53:57 +0100

    ReadCustomer

--- HotelBooking/Controllers/Read/ReadCustomer.cs
+++ HotelBooking/Controllers/Read/ReadCustomer.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HotelBooking.Data;
 
-namespace HotelBooking.Controllers.Read
+namespace HotelBooking.Controllers.Read;
+
+public class ReadCustomer : ICrud
 {
-    public class ReadCustomer
+    public ReadCustomer(ApplicationDbContext dbContext) //skicka in dbcontext i ctor eller in run metod?
     {
+        DatabaseContext = dbContext;
+    }
+
+    public ApplicationDbContext DatabaseContext { get; set; }
+
+    public void RunCrud()
+    {
+        using (DatabaseContext)
+        {
+            Console.WriteLine("(Visa alla kunder");
+            Console.WriteLine("=================");
+            ViewCustomers();
+        }
+    }
+
+    public void ViewCustomers()
+    {
+        foreach (var customer in DatabaseContext.Customers)
+            Console.WriteLine(
+                $"Id{customer.CustomerId}: {customer.FirstName} {customer.LastName} Telefon: {customer.Phone} ");
     }
 }
