6c86ddb36eb9faaaf83dbcc7d7716599d22c516b
malin <mal3eine@hotmail.com>
2023-01-01 18:13:49 +0100

    SQL SubQuery

--- Extra files/HotelQuery.sql
+++ Extra files/HotelQuery.sql
@@ -1,19 +1,68 @@
 USE Hotel;
 
---Show bookings ordered by booking id
+--Show bookings ordered by booking id------------------------------------------
 SELECT 
 	b.BookingId, 
 	c.FirstName, 
 	c.LastName,  
 	r.RoomId, r.[Type], 
 	r.SizeSquareMeters 
-FROM Customers c
-INNER JOIN Bookings b ON c.CustomerId=b.CustomerId
-INNER JOIN Rooms r ON b.RoomId=r.RoomId
-ORDER BY b.BookingId;
+FROM 
+	Customers c
+INNER JOIN 
+	Bookings b ON c.CustomerId=b.CustomerId
+INNER JOIN 
+	Rooms r ON b.RoomId=r.RoomId
+ORDER BY 
+	b.BookingId;
 
---Show all rooms ordered by number of guests
-SELECT *
-FROM Rooms
-WHERE SizeSquareMeters > 20
-ORDER BY NumberOfGuests DESC
+--Show bookings/room-----------------------------------------------------------
+SELECT 
+	RoomId,
+	COUNT(*) AS NumberOfBookings
+FROM
+	Bookings
+GROUP BY
+	RoomId;
+
+--Show all rooms bigger than 20 square meters ordered by number of guests------
+SELECT 
+	*
+FROM 
+	Rooms
+WHERE 
+	SizeSquareMeters > 20
+ORDER BY 
+	NumberOfGuests DESC;
+
+--Show all rooms and average room size-----------------------------------------
+SELECT
+	RoomId,
+	[Type],
+	SizeSquareMeters, 
+		(
+		SELECT 
+			AVG(SizeSquareMeters) 
+		FROM 
+			Rooms
+		) AS AverageRoomSize
+FROM 
+	Rooms;
+
+--Show all bookings where days booked are more than average--------------------
+SELECT
+	BookingId,
+	NumberOfDays,
+	RoomId,
+	CONVERT(char(10), StartDate, 23) AS StartDate,
+	CONVERT(char(10), EndDate, 23) AS EndDate
+FROM 
+	Bookings
+WHERE 
+	NumberOfDays > 
+		(
+		SELECT 
+			AVG(NumberOfDays) 
+		FROM 
+			Bookings
+		);
