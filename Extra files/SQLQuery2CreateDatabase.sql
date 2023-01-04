b6e9fe9400bd82277379880fce374bb4e562b8f0
malin <mal3eine@hotmail.com>
2023-01-04 17:31:06 +0100

    EXTRA FILES QUERIES

---
+++ Extra files/SQLQuery2CreateDatabase.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE Hotel;
+
+CREATE TABLE Bookings(
+[BookingId] int NOT NULL PRIMARY KEY,
+[CustomerId] int NOT NULL FOREIGN KEY REFERENCES Customers(CustomerId),
+[RoomId] int NOT NULL FOREIGN KEY REFERENCES Rooms(RoomId),
+[StartDate] datetime2(7) NOT NULL,
+[EndDate] datetime2(7) NOT NULL,
+[NumberOfDays] int NOT NULL,
+[GuestCount] int NOT NULL,
+);
+
+CREATE TABLE Customers(
+[CustomerId] int NOT NULL PRIMARY KEY,
+[FirstName] nvarchar(100) NOT NULL,
+[LastName] nvarchar(100) NOT NULL,
+[Phone] nvarchar(20) NOT NULL,
+[Active] bit NOT NULL,
+);
+
+CREATE TABLE Rooms(
+[RoomId] int NOT NULL PRIMARY KEY,
+[SizeSquareMeters] int NOT NULL,
+[NumberOfGuests] int NOT NULL,
+[Type] nvarchar(10) NOT NULL,
+[ExtraBed] int NOT NULL,
+);
+
+-----------------------------------------
